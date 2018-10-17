ac7012f997fa03da8ca1bd4565d04cd41a3fa528
Bramonsrike <44245683+Bramonsrike@users.noreply.github.com>
2018-10-17 23:32:32 +0200

    Add files via upload

---
+++ FollowCursor_shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor_shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    //private Rigidbody2D rb2d;
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("mouse 0"))
+        {
+            shoot();
+        }
+	}
+
+    void shoot()
+    {
+        //rb2d = bullet.GetComponent<Rigidbody2D>();
+        //Vector2 playerpos = Camera.main.WorldToScreenPoint();
+        //rb2d.AddForce(Vector2.right*2);
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+
+}
---
+++ movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    private Vector2 position;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("d"))
+        {
+            position.x += speed;
+            transform.position = position;
+        }
+        if (Input.GetKey("q"))
+        {
+            position.x -= speed;
+            transform.position = position;
+        }
+        if (Input.GetKey("z"))
+        {
+            position.y += speed;
+            transform.position = position;
+        }
+        if (Input.GetKey("s"))
+        {
+            position.y -= speed;
+            transform.position = position;
+        }
+    }
+}
